27d3058b224d0a1d2ab40f0c3049290e722db5c3
Teamtop <1247robotics@gmail.com>
2024-02-05 15:54:19 -0500

    easier

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -117,7 +117,8 @@ public class Robot extends TimedRobot {
   @Override
   public void testPeriodic() {
     time = (int) System.currentTimeMillis() / 1000;
-    climb.set(time % 2 == 0 ? 0.5 : -0.5);
+    double spee = 1;
+    climb.set(time % 2 == 0 ? spee : -spee);
   }
 
   /** This function is called once when the robot is first started up. */
